2d0b08b99a611c060a7a064563fd6f0fd71a5925
bviktoria <email>
2025-04-22 09:36:50 +0200

    Start implementing shell command

--- src/main/java/com/esen/bookstore/data/DataLoader.java
+++ src/main/java/com/esen/bookstore/data/DataLoader.java
@@ -1,6 +1,7 @@
 package com.esen.bookstore.data;
 
 import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.Bookstore;
 import com.esen.bookstore.reopsitory.BookRepository;
 import com.esen.bookstore.reopsitory.BookStoreRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -16,20 +17,16 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
-
+@RequiredArgsConstructor
 public class DataLoader {
+
     private final BookRepository bookRepository;
     private final BookStoreRepository bookStoreRepository;
-
-    public DataLoader(BookRepository bookRepository, BookStoreRepository bookStoreRepository) {
-        this.bookRepository = bookRepository;
-        this.bookStoreRepository = bookStoreRepository;
-    }
-
-
 
     @Value("classpath:data/books.json")
     private Resource booksResource;
@@ -37,7 +34,7 @@ public class DataLoader {
     @Value("classpath:data/bookstores.json")
     private Resource bookstoreResource;
 
-    @PostConstruct //letrehozas utan futassa le metodus
+    @PostConstruct //letrehozas utan futassa le metodust
     public void loadData() {
         var objectMapper = new ObjectMapper();
 
@@ -46,6 +43,19 @@ public class DataLoader {
             var books = objectMapper.readValue(booksJson, new TypeReference<List<Book>>() {});
             bookRepository.saveAll(books);
 
+            var bookstoreJson = StreamUtils.copyToString(bookstoreResource.getInputStream(), StandardCharsets.UTF_8);
+            var bookstores = objectMapper.readValue(bookstoreJson, new TypeReference<List<Bookstore>>() {
+            });
+
+            bookstores.forEach(bookstore -> bookstore.setInventory(books.stream()
+                        .collect(Collectors.toMap(book -> book,
+                                book -> ThreadLocalRandom.current().nextInt(1,50))
+                        )
+            ));
+
+            bookStoreRepository.saveAll(bookstores);
+
+            log.info("successfully loadaed entities to the database");
         } catch (IOException e) {
             System.out.println("Cannot seed database" + e);
         }
--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 
-
+@Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
@@ -18,54 +21,4 @@ public class Book {
     private String publisher;
     private Double price;
 
-    public Book() {
-    }
-
-    public Book(Long id, String title, String author, String publisher, Double price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
